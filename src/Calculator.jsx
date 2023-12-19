cd40a55debd446d4a2d94a2ad0eccc5f6d17cc3d
AbyG1 <abygeorge133@gmail.com>
2023-12-19 08:48:54 +0530

    Add calcultate function

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -4,10 +4,36 @@ import Col from 'react-bootstrap/Col';
 import Slider from '@mui/material/Slider';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
+
+
+
+
+
 
 
 function Calculator(){
 
+    const [originalPrice ,setOriginalPrice] = useState(0);
+    const [discount,setDiscount] = useState(0)
+    const [finalPrice,setFinalPrice] = useState(0);
+    const [savedPrice,setSavedPrice] = useState(0)
+
+    console.log(originalPrice)
+    console.log(discount)
+
+    const calcultaeDiscout = (e) => {
+        let priceYouSave = originalPrice * discount / 100
+        let priceToPay = originalPrice - priceYouSave
+
+        setFinalPrice(priceToPay)
+        setSavedPrice(priceYouSave)
+    }
+
+    const resetValue = (e) => {
+        setOriginalPrice(0)
+        
+    }
 
     return(
 
@@ -18,28 +44,31 @@ function Calculator(){
                     <div className="col-lg-4 mb-5 ">
                         <div className='text-center mb-5'>
                             <h2 className='text-light'>Original price</h2>
-                            <TextField id="outlined-basic" label="price" variant="outlined" className='yellow' />
+                            <TextField type='number' onChange={(e) => setOriginalPrice(e.target.value)} id="outlined-basic"  variant="outlined" className='yellow' />
                         </div>
                         <div className='text-center mb-2'>
                             <h2 className='text-light'>Discount</h2>
-                            <h2 className='text-light'>{}%</h2>
-                            <Slider defaultValue={0} aria-label="Default" valueLabelDisplay="auto" className='text-warning'/> 
+                            <h2 className='text-light'>{discount}%</h2>
+                            <Slider defaultValue={0} onChange={(e) => setDiscount(e.target.value)} aria-label="Default"  className='text-warning'/> 
                         </div>
                         <div className='text-center'>
-                            <Button variant="contained" className='bg-warning'>Calculate</Button>
+                            <Button variant="contained" onClick={e => calcultaeDiscout(e)} className='bg-warning'>Calculate</Button>
                         </div>
                         
                     </div>
-                    <div className="col-lg-4  display rounded">
+                    <div className="col-lg-4   ">
 
-                            <div className="text-center text-dark">
+                            <div className="display rounded text-center text-dark">
 
                                 <h2>Final Price</h2>
-                                <p className='display-3'>33</p>
+                                <p className='display-3'>{finalPrice}</p>
                                 <h2>You Save</h2>
-                                <p className='display-3'>33</p>
+                                <p className='display-3'>{savedPrice}</p>
+                                
 
-
+                            </div>
+                            <div className='text-center'>
+                                <Button variant="outlined" onClick={e => resetValue(e)}>Reset</Button>
                             </div>
 
 
